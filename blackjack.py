0a1ea9b1faff19f98ad80d0dcf49c968d36eb551
Davichet-e <48657914+Davichet-e@users.noreply.github.com>
2019-04-15 11:37:58 +0200

    Modularized some functions and implemented the hand class

--- blackjack.py
+++ blackjack.py
@@ -4,18 +4,16 @@ Author: David García Morillo
 '''
 from time import sleep
 
-from blackjack_player import BlackJackPlayer, get_my_deck
-from deck import Deck
+from blackjack_player import BlackJackPlayer
+from hand import BlackJackHand
 
 
-def blackjack() -> None:
+def blackjack():
     """A multiplayer 21 BlackJack simulator. Just call this function, and enjoy!"""
 
     check_first_game = True
-    my_deck = get_my_deck()
     players = []
-    dealer_cards = my_deck.get_initial_cards()
-    dealer_points = Deck.sum_points(dealer_cards)
+    dealer = BlackJackHand()
 
     #---------------------------------------------------------------------------------#
     def initialization():
@@ -25,51 +23,59 @@ def blackjack() -> None:
             "\t\t\t\t#########################################\tGame Started\t   \
 #########################################")
         if check_first_game:
+            check_first_game = False
             print(
                 "\t\t\t\tThis BlackJack Game has been created by David Garcia Morillo"
             )
-            check_first_game = False
+            number_of_people = ask_number_of_people()
+            ask_player_attributes(number_of_people)
+        game()
+
+    def ask_number_of_people() -> int:
+        while True:
+            try:
+                number_of_people = int(
+                    input(
+                        "\t\t\t\tHow many people are going to play? (1-5)\n\t\t\t\t"
+                    ))
+            except ValueError:
+                print("\t\t\t\tPlease, use only numbers.\n")
+            else:
+                if not 0 < number_of_people <= 5:
+                    print(
+                        "\t\t\t\tThe number of people must be between 1 and 5\n"
+                    )
+                    continue
+                break
+        return number_of_people
+
+    def ask_player_attributes(num_of_people: int):
+        for i in range(num_of_people):
+            name = input(
+                f"\t\t\t\tPlease, enter your name, Player {str(i + 1)}\n\t\t\t\t"
+            )
             while True:
                 try:
-                    number_of_people = int(
+                    initial_money = int(
                         input(
-                            "\t\t\t\tHow many people are going to play? (1-5)\n\t\t\t\t"
+                            "\t\t\t\tHow much money do you have? (Use only numbers)\n\t\t\t\t"
                         ))
                 except ValueError:
                     print("\t\t\t\tPlease, use only numbers.\n")
                 else:
-                    if not 0 < number_of_people <= 5:
+                    if initial_money < 0:
                         print(
-                            "\t\t\t\tThe number of people must be between 1 and 5\n"
+                            "\t\t\t\tThe initial money must be greater than 0\n"
                         )
                         continue
+                    players.append(BlackJackPlayer(name, initial_money))
                     break
-            for i in range(number_of_people):
-                name = input(
-                    f"\t\t\t\tPlease, enter your name, Player {str(i + 1)}\n\t\t\t\t"
-                )
-                while True:
-                    try:
-                        initial_money = int(
-                            input(
-                                "\t\t\t\tHow much money do you have? (Use only numbers)\n\t\t\t\t"
-                            ))
-                    except ValueError:
-                        print("\t\t\t\tPlease, use only numbers.\n")
-                    else:
-                        if initial_money < 0:
-                            print(
-                                "\t\t\t\tThe initial money must be greater than 0\n"
-                            )
-                            continue
-                        players.append(BlackJackPlayer(name, initial_money))
-                        break
-        game()
 
     def game():
         print("\t\t\t\tThe game has started.\n")
         print(
-            f"\t\t\t\tThe first card of the dealer is {str(dealer_cards[0])}")
+            f"\t\t\t\tThe first card of the dealer is {str(dealer.get_cards()[0])}"
+        )
         for player in players:
             player_turn(player)
         dealer_turn()
@@ -109,20 +115,23 @@ def blackjack() -> None:
         print("\t\t\t\tNow, your cards are:", end=" ")
         player_cards = player.get_cards()
         for card in player_cards:
-            if player_cards.index(card) != (len(player_cards) - 1):
-                print(card, end=", ")
-            else:
-                print(card, end=".\n")
+            if player_cards[-1] != card:  # While the card is not the last,
+                print(card, end=", ")  # separate them with comas,
+            else:  # otherwise, print the final point
+                print(card, end=".\n")  # and a jumpline.
+
+    def check_if_yes(user_decision: str) -> bool:
+        return user_decision.strip().lower() == "y"\
+            or user_decision.strip().lower() == "yes"\
+            or user_decision.strip().lower() == "true"\
+            or user_decision.strip().lower() == "1"
 
     def ask_if_hit(player: BlackJackPlayer):
         for _ in range(21):
             sleep(2)
             if player_win_lose_condition(player) is None:
                 decision = input("\t\t\t\tDo you wanna hit? (y/n)\n\t\t\t\t")
-                if decision.strip().lower() == "y"\
-                        or decision.strip().lower() == "yes"\
-                        or decision.strip().lower() == "true"\
-                        or decision.strip().lower() == "1":
+                if check_if_yes(decision):
                     player.deal_card()
                     show_player_cards(player)
                 else:
@@ -149,24 +158,21 @@ def blackjack() -> None:
         ask_if_hit(player)
 
     def dealer_win_lose_condition() -> bool:
-        nonlocal dealer_points
-        if dealer_points > 21:
+        if not dealer.get_points():
             print("\t\t\t\tThe dealer busted. The game ended :)\n")
-            dealer_points = 1
             return True
         return False
 
     def show_dealer_cards():
-        nonlocal dealer_cards
         print("\t\t\t\tThe dealer cards are: ", end="")
+        dealer_cards = dealer.get_cards()
         for card in dealer_cards:
-            if dealer_cards.index(card) != (len(dealer_cards) - 1):
+            if dealer_cards[-1] != card:
                 print(card, end=", ")
             else:
                 print(card, end=".\n")
 
     def dealer_turn():
-        nonlocal dealer_cards, dealer_points
         print(
             "\t\t\t\t#########################################\tDealer's Turn\t   \
 #########################################")
@@ -175,10 +181,9 @@ def blackjack() -> None:
             sleep(2)
             show_dealer_cards()
             if not dealer_win_lose_condition():
-                if dealer_points < 17:
+                if dealer.get_points() < 17:
                     print("\t\t\t\tThe dealer is going to hit a card\n")
-                    dealer_cards.append(my_deck.deal_card())
-                    dealer_points = Deck.sum_points(dealer_cards)
+                    dealer.deal_card()
                 else:
                     print()
                     break
@@ -186,28 +191,47 @@ def blackjack() -> None:
                 print()
                 break
 
-    def reset_dealer_cards():
-        nonlocal dealer_cards, dealer_points
-        dealer_cards = my_deck.get_initial_cards()
-        dealer_points = Deck.sum_points(dealer_cards)
-
     def end_game():
         print(
             "\t\t\t\t#########################################\tResults\t      \
 ##############################################")
         for player in players:
             if player.get_points() == 21\
-                    or player.get_points() > dealer_points:
+                    or player.get_points() > dealer.get_points():
                 player.update_actual_money(player.get_actual_bet())
                 print(
                     f"\t\t\t\t{str(player)} won {str(player.get_actual_bet() * 2)}€ :)\n"
                 )
-            elif player.get_points() == dealer_points:
-                print(f"\t\t\t\t{str(player)}, it is a Tie! :|\n")
-            else:
-                player.update_actual_money(-player.get_actual_bet())
+            elif not player.get_points()\
+                or player.get_points() < dealer.get_points():        # Checks if the player's points are 0 
+                player.update_actual_money(-player.get_actual_bet()) # or if they are less than the dealer's points  
                 print(f"\t\t\t\t{str(player)} lost against the dealer :(\n")
+            else:
+                print(f"\t\t\t\t{str(player)}, it is a Tie! :|\n")
         sleep(1)
+
+    def ask_if_reset(player: BlackJackPlayer):
+        player_reset: bool = None
+        final_balance = f"{str(player.get_actual_money() - player.get_initial_money())} €"
+        if "-" not in final_balance:
+            final_balance = "+" + final_balance
+        if player.get_actual_money() > 0:
+            decision = input(
+                f"\t\t\t\t{str(player)}, do you want to play again? (y/n)\n\t\t\t\t"
+            )
+            if check_if_yes(decision):
+                player.reset_cards()
+                player_reset = True
+            else:
+                print(f"\t\t\t\tThanks for playing, {str(player)}\
+ your final balance is {final_balance}\n")
+                player_reset = False
+        else:
+            print(
+                f"\t\t\t\t{str(player)}, you have lost all your money. Thanks for playing\n"
+            )
+            player_reset = False
+        return player_reset
 
     def reset():
         print(
@@ -215,28 +239,7 @@ def blackjack() -> None:
 #########################################")
         delete_players = []
         for player in players:
-            final_balance = f"{str(player.get_actual_money() - player.get_initial_money())} €"
-            if "-" not in final_balance:
-                final_balance = "+" + final_balance
-            if player.get_actual_money() > 0:
-                decision = input(
-                    f"\t\t\t\t{str(player)}, do you want to play again? (y/n)\n\t\t\t\t"
-                )
-                if decision.strip().lower() == "y"\
-                        or decision.strip().lower() == "yes"\
-                        or decision.strip().lower() == "true"\
-                        or decision.strip().lower() == "1":
-                    player.reset_cards()
-                else:
-                    print(
-                        f"\t\t\t\tThanks for playing, {str(player)}\
-your final balance is {final_balance}\n"
-                    )
-                    delete_players.append(player)
-            else:
-                print(
-                    f"\t\t\t\t{str(player)}, you have lost all your money. Thanks for playing\n"
-                )
+            if not ask_if_reset(player):
                 delete_players.append(player)
 
         for player_to_delete in delete_players:
@@ -245,7 +248,7 @@ your final balance is {final_balance}\n"
         print("\n\n\n\n\n")
 
         if players:
-            reset_dealer_cards()
+            dealer.reset_cards()
             initialization()
 
     #---------------------------------------------------------------------------------#
