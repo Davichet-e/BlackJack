afc4fb0bef64225e89ff3ef7d9e8b0e62275a185
Davichet-e <48657914+Davichet-e@users.noreply.github.com>
2019-04-23 01:26:20 +0200

    Fixed some minor bugs

--- deck.py
+++ deck.py
@@ -1,87 +1,94 @@
 """
 This module implements the standard 52-card deck and its correspondents cards
+Author: David García Morillo
 """
 
 from random import shuffle
+from typing import Dict, List, Tuple
 
 
-class Card():
+class Card:
     """This class implements the cards of the standard 52-card deck"""
 
     def __init__(self, name: str, suit: str, value: int):
-        self.__name = name
-        self.__suit = suit
-        self.__value = value
+        self._name: str = name
+        self._suit: str = suit
+        self._value: int = value
 
-    def __str__(self):
-        return f"{self.__name} of {self.__suit}"
+    def __repr__(self) -> str:
+        return f"{self._name} of {self._suit}"
 
-    def get_name(self) -> str:
+    @property
+    def name(self) -> str:
         """Returns the name of the card"""
-        return self.__name
+        return self._name
 
-    def get_value(self) -> int:
+    @property
+    def value(self) -> int:
         """Returns the value of the card"""
-        return self.__value
+        return self._value
 
-    def get_suit(self) -> str:
+    @property
+    def suit(self) -> str:
         """Returns the suit of the card"""
-        return self.__suit
+        return self._suit
 
 
-class Deck():
-    '''This class implements the standard 52-card deck'''
+class Deck:
+    """This class implements the standard 52-card deck"""
 
-    SUITS = ("♣", "♥", "♠", "♦")
-    __CARDS = dict(ACE=11,
-                   TWO=2,
-                   THREE=3,
-                   FOUR=4,
-                   FIVE=5,
-                   SIX=6,
-                   SEVEN=7,
-                   EIGHT=8,
-                   NINE=9,
-                   TEN=10,
-                   JACK=10,
-                   QUEEN=10,
-                   KING=10)
-    #A copy of the cards, created for prevent a player to change or add any element
-    PUBLIC_CARDS = __CARDS.copy()
+    SUITS: Tuple[str] = ("♣", "♥", "♠", "♦")
+    _CARDS: Dict[str, int] = dict(
+        ACE=11,
+        TWO=2,
+        THREE=3,
+        FOUR=4,
+        FIVE=5,
+        SIX=6,
+        SEVEN=7,
+        EIGHT=8,
+        NINE=9,
+        TEN=10,
+        JACK=10,
+        QUEEN=10,
+        KING=10,
+    )
+    # A copy of the cards, created for prevent a player to change or add any element
+    PUBLIC_CARDS: Dict[str, int] = _CARDS.copy()
 
     def __init__(self):
-        self.__deck = []
+        self._deck: List[Card] = []
         for suit in self.SUITS:
-            for card in self.__CARDS.items():
-                self.__deck.append(Card(card[0], suit, card[1]))
-        shuffle(self.__deck)
+            for card in self._CARDS.items():
+                self._deck.append(Card(card[0], suit, card[1]))
+        shuffle(self._deck)
 
-    def __str__(self):
-        cards_repr = ""
-        for card in self.__deck:
-            cards_repr += f"\n{card.__str__()}"
+    def __repr__(self) -> str:
+        cards_repr: str = ""
+        for card in self._deck:
+            cards_repr += f"\n{card.__repr__()}"
         return f"Deck cards:\n{cards_repr}"
 
     def __len__(self):
-        return len(self.__deck)
+        return len(self._deck)
 
     def deal_card(self) -> Card:
         """Returns a random card of the deck."""
-        top = self.__deck[0]
-        self.__deck.remove(top)
+        top: Card = self._deck[0]
+        self._deck.remove(top)
         return top
 
-    def get_initial_cards(self) -> list:
+    def get_initial_cards(self) -> List[Card]:
         """Returns 2 random cards of the deck"""
-        initial_cards = self.__deck[0:2]
-        for i in initial_cards:
-            self.__deck.remove(i)
+        initial_cards: List[Card] = self._deck[0:2]
+        for card in initial_cards:
+            self._deck.remove(card)
         return initial_cards
 
     @staticmethod
-    def sum_points(cards: list) -> int:
+    def sum_points(cards: List[Card]) -> int:
         """Sum the points of the list of cards which are received as a parameter"""
-        points = 0
+        points: int = 0
         for card in cards:
-            points += card.get_value()
+            points += card.value
         return points
