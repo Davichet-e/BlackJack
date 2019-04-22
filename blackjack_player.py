cf4fc644954deebe3303abbab28d9b46c7b5cc69
Davichet-e <48657914+Davichet-e@users.noreply.github.com>
2019-04-23 01:25:09 +0200

    Added asserts for type checking

--- blackjack_player.py
+++ blackjack_player.py
@@ -1,14 +1,17 @@
-'''
+"""
 Author: David García Morillo
-'''
-from player import Player
+"""
 from hand import BlackJackHand
+from player import Player
 
 
 class BlackJackPlayer(Player, BlackJackHand):
     """
     TODO
     """
+
     def __init__(self, name: str, initial_money: int):
+        assert isinstance(name, str), "The type of 'name' must be str"
+        assert isinstance(initial_money, int), "The type of 'initial_money' must be str"
         Player.__init__(self, name, initial_money)
         BlackJackHand.__init__(self)
