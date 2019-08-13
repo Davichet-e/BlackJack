bcde8a13557406ce33b7450762b5ab04f1c3a098
David Garcia <davidgmorillop@gmail.com>
2019-08-14 00:05:40 +0200

    Change multi-inheritance to composition

--- blackjack_player.py
+++ blackjack_player.py
@@ -7,15 +7,15 @@ from blackjack_hand import BlackJackHand
 from player import Player
 
 
-class BlackJackPlayer(Player, BlackJackHand):
+class BlackJackPlayer(Player):
     """This class uses the 'Player' and 'BlackJackHand' classes 
     to create a BlackJack player model in python.
     """
 
     def __init__(self, name: str, initial_money: int):
         Player.__init__(self, name, initial_money)
-        BlackJackHand.__init__(self)
+        self._hand: BlackJackHand = BlackJackHand()
 
     @property
-    def hand(self) -> str:
-        return BlackJackHand.__str__(self)
+    def hand(self) -> BlackJackHand:
+        return self._hand
